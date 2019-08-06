b4051c2850543d14e37a9062c2562f0aacdee9a4
Isaac Mackle <mackle@Isaacs-MacBook-Air.local>
2019-08-06 18:58:43 +1200

    Final Touchups

--- main.js
+++ main.js
@@ -3,4 +3,12 @@ $(document).ready(() => {
 		$(event.currentTarget).child('.info').show();
 	})
 
+	$('.footer').on('mouseenter', event => {
+		$(event.currentTarget).child('.foot-text').removeClass('hide');
+	})
+
+	$('.footer').on('mouseleave', event => {
+		$(event.currentTarget).child('.foot-text').addClasss('hide');
+	})
+
 })
